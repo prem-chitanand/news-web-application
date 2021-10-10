712aa7f2ee1d6c345fc2b03abaf008219317a9d4
prem-chitanand <prem.chitanand@gmail.com>
2021-10-10 15:41:18 +0530

    news api web application

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-// b047a4ac6414432cb675dd813cd00584
-
 let country=document.getElementById('countryCode');
 let submit=document.getElementById('submit').onclick=function(){
     let value=country.value;
-    console.log(value);
+    if(value.length!=0){
   let apiKey="b047a4ac6414432cb675dd813cd00584";
 let news=fetch(`https://newsapi.org/v2/top-headlines?country=${country.value}&apiKey=${apiKey}`)
 .then(response => response.json())
 .then(data => {
 let articles=data.articles; 
-let heading=`<h1> Showing Live updates regarding country :${value}</h1>`  
+let heading=`<h1> Showing Live News updates regarding country :${value}</h1>`  
 let newHTML=""
 articles.forEach(element => {
-    console.log(element["author"])
-    console.log(element["title"])
-    console.log(element["description"])
-    console.log(element["publishedAt"])
     let newsHtml=`<div class="row">
     <div class="col-sm-6 col-md-4 col-lg-10">
     
       <div class="thumbnail">
       <b><h3>Title:${element["title"]}</h3></b>
+      <hr>
         <div class="caption">
-          <h3>The author:${element["author"]}</h3>
+          <h3>The author:<b>${element["author"]}</b></h3>
           <p>Description:${element["description"]}</p>
-          <p>Published at:${element["publishedAt"]}</p>
+          <hr>
+         <b> <p>Published at:${element["publishedAt"]}</p><b>
         </div>
       </div>
     </div>
@@ -36,6 +32,11 @@ articles.forEach(element => {
   document.getElementById('thumbnail').innerHTML=newHTML;
 })
 country.value=""
+
+}
+else{
+    window.alert(" please enter country code");
+}
 
 }
 
